364cbe299db5e8f07e1c33648104a7ef0326aef0
Gabe Wilberscheid <wilberscheidgabe@gmail.com>
2020-05-26 11:05:29 -0500

    Revert "Django blog base"
    This reverts commit 48f08ab48d48926a643266e805ab22d1096154e7.

--- DjangoPractice/django_project/polls/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='poll-home'),
-    path('about/', views.about, name='poll-about'),
-]
--- DjangoPractice/django_project/users/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersConfig(AppConfig):
-    name = 'users'
-
-    # this links to functions that create a profile everytime a user is created and links them together
-    def ready(self):
-        import users.signals
